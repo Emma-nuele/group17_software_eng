c24383f40e98ef3adf86c9b28c79fc3907b89d45
CaterinaNugnes0 <151541009+CaterinaNugnes0@users.noreply.github.com>
2023-12-11 13:00:51 +0100

    Operations.java

--- Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Operations.java
+++ Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Operations.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package calcolatrice.developed;
+package calculator.developed;
 
-import calcolatrice.exceptions.*;
+import calculator.exceptions.*;
 
 /**
  *
