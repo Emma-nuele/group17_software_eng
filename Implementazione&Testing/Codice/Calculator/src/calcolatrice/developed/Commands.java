2a761a5b886454804ba9d88c2b94abcf32acd2fc
CaterinaNugnes0 <151541009+CaterinaNugnes0@users.noreply.github.com>
2023-12-11 12:59:57 +0100

    Commands.java

--- Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Commands.java
+++ Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Commands.java
@@ -2,10 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package calcolatrice.developed;
+package calculator.developed;
 
-import calcolatrice.exceptions.FullStackException;
-import calcolatrice.exceptions.InsuffElemStackException;
+import calculator.exceptions.FullStackException;
+import calculator.exceptions.InsuffElemStackException;
 
 /**
  *
