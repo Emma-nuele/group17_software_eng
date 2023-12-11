414fcfd8ff86540e69920b96c1fff2a5c8c4a977
Emma-nuele <151515111+Emma-nuele@users.noreply.github.com>
2023-12-11 10:52:40 +0100

    Update CalculatorViewController.java

--- Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/CalculatorViewController.java
+++ Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/CalculatorViewController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator.developed;
 
 import java.net.URL;
@@ -14,10 +9,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
-/**
- *
- * @author cater
- */
+
 public class CalculatorViewController {
 
     @FXML
