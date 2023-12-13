08f5252ecbff9f67d174df629508c925713ef92e
Emma-nuele <151515111+Emma-nuele@users.noreply.github.com>
2023-12-13 22:47:55 +0100

    Update Operations.java

--- Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Operations.java
+++ Implementazione&Testing/Codice/Calculator/src/calcolatrice/developed/Operations.java
@@ -1,143 +1,138 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package calculator.developed;
 
+
 import calculator.exceptions.*;
 
-public class Operations{
-    
+public class Operations {
+
     private StackCalc stack;
-    
-    public Operations(StackCalc stack){
-        
+
+    public Operations(StackCalc stack) {
+
         this.stack = stack;
-        
+
     }
-    
-    public void sum() throws InsuffElemStackException, FullStackException{
-        
-        if(!stack.leastTwo())
+
+    public void sum() throws InsuffElemStackException, FullStackException {
+
+        if (!stack.leastTwo()) {
             throw new InsuffElemStackException("Impossibile eseguire comando, non ci sono abbastanza elementi nello stack");
-        
-        
+        }
+
         Number ultimo = stack.pop();
         Number penultimo = stack.pop();
-        
-        Number risultato = new Number( ultimo.getRe() + penultimo.getRe(), ultimo.getIm() + penultimo.getIm());
-       
+
+        Number risultato = new Number(ultimo.getRe() + penultimo.getRe(), ultimo.getIm() + penultimo.getIm());
+
         stack.push(risultato);
-                
+
     }
-    
-    public void sub() throws InsuffElemStackException,FullStackException{
-        
-        if(!stack.leastTwo())
+
+    public void sub() throws InsuffElemStackException, FullStackException {
+
+        if (!stack.leastTwo()) {
             throw new InsuffElemStackException("Impossibile eseguire comando, non ci sono abbasatnza elementi nello stack");
-        
-        
+        }
+
         Number ultimo = stack.pop();
         Number penultimo = stack.pop();
-        
-        Number risultato = new Number( penultimo.getRe() - ultimo.getRe(), penultimo.getIm() - ultimo.getIm());
-        
+
+        Number risultato = new Number(penultimo.getRe() - ultimo.getRe(), penultimo.getIm() - ultimo.getIm());
+
         stack.push(risultato);
-        
-        
+
     }
-    
-    public void multiplication() throws InsuffElemStackException, FullStackException{
-        
-        if(!stack.leastTwo())
+
+    public void multiplication() throws InsuffElemStackException, FullStackException {
+
+        if (!stack.leastTwo()) {
             throw new InsuffElemStackException("Impossibile eseguire comando, non ci sono abbasatnza elementi nello stack");
-        
-        
+        }
+
         Number ultimo = stack.pop();
         Number penultimo = stack.pop();
-        
+
         double risultatoRe = (penultimo.getRe() * ultimo.getRe()) - (ultimo.getIm() * penultimo.getIm());
         double risultatoIm = (penultimo.getRe() * ultimo.getIm()) + (ultimo.getRe() * penultimo.getIm());
-        Number risultato = new Number( risultatoRe , risultatoIm);
-        
+        Number risultato = new Number(risultatoRe, risultatoIm);
+
         stack.push(risultato);
-        
+
     }
-    
-    
-    public void division() throws DivisionZeroException, InsuffElemStackException, FullStackException{
-        
-        if(!stack.leastTwo())
+
+    public void division() throws DivisionZeroException, InsuffElemStackException, FullStackException {
+
+        if (!stack.leastTwo()) {
             throw new InsuffElemStackException("Impossibile eseguire comando, non ci sono abbasatnza elementi nello stack");
-        
-        
+        }
+
         Number ultimo = stack.pop();
         Number penultimo = stack.pop();
-        
+
         double a = penultimo.getRe();
         double b = penultimo.getIm();
         double c = ultimo.getRe();
         double d = ultimo.getIm();
-        
-        if((c == 0 && d == 0))
+
+        if ((c == 0 && d == 0)) {
             throw new DivisionZeroException("\nImpossibile dividere per zero");
-        
-            double risultatoRe = ( (a*c) + (b*d) )/( (c*c) + (d*d) );
-            double risultatoIm = ( (b*c) - (a*d) )/( (c*c) + (d*d) );
-            Number risultato = new Number( risultatoRe , risultatoIm);
-            stack.push(risultato);
-       
-        
-        
+        }
+
+        double risultatoRe = ((a * c) + (b * d)) / ((c * c) + (d * d));
+        double risultatoIm = ((b * c) - (a * d)) / ((c * c) + (d * d));
+        Number risultato = new Number(risultatoRe, risultatoIm);
+        stack.push(risultato);
+
     }
-    
-    
-    public void sqrt() throws FullStackException, InsuffElemStackException{
-        
+
+    public void sqrt() throws FullStackException, InsuffElemStackException {
+
         Number val = stack.pop();
-        
-        if (val.getIm() == 0){ //controllo se il valore è reale
-                
-            if (val.getRe() >= 0){ //se è maggiore di zero eseguo la radice classica
-                
+
+        if (val.getIm() == 0) { //controllo se il valore è reale
+
+            if (val.getRe() >= 0) { //se è maggiore di zero eseguo la radice classica
+
                 val.setRe(Math.sqrt((val.getRe())));
-                
-                    
-            }else{
-                
+
+            } else {
+
                 val.setIm(Math.sqrt((-1 * val.getRe()))); //se è minore di zero eseguo la radice del valore trasformato in positivo
-                
+
                 val.setRe(0);             //e lo inserisco come valore immaginario e setto la parte reale a 0
-                    
+
             }
-            
-        }else{
-            
+
+        } else {
+
             double abs = Math.sqrt(val.getRe() * val.getRe() + val.getIm() * val.getIm());   //calcolo modulo e fase
             double arg = (2 * Math.PI + Math.atan2(val.getIm(), val.getRe())) % (2 * Math.PI);
 
             val.setRe(Math.sqrt(abs) * (Math.cos((arg / 2))));        //calcolo i valori reali e immaginari e li inserisco nell'oggetto val
             val.setIm(Math.sqrt(abs) * (Math.sin(arg / 2)));
-            
-            }
 
-        
+        }
+
         stack.push(val);
-        
+
     }
-    
-    
-    public void signReversal ()throws InsuffElemStackException, FullStackException{
-        
-        
+
+    public void signReversal() throws InsuffElemStackException, FullStackException {
+
         Number val = stack.pop();
-        
-        val.setIm(val.getIm()*-1);
-        val.setRe(val.getRe()*-1);
-        
+
+        if(val.getIm()!= 0)
+        val.setIm(val.getIm() * -1);
+        if(val.getRe()!= 0)
+        val.setRe(val.getRe() * -1);
+
         stack.push(val);
-        
-        
+
     }
-    
+
 }
